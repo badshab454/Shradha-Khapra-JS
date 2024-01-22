e25ccc8105a972c151ab423fe12ea422c23e6d46
Badsha Hossain <badshab454@gmail.com>
2024-01-22 17:02:58 +0600

    slice

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-function smallestSubstring(S) {
-    let count = {'0': 0, '1': 0, '2': 0};
-    let start = 0;
-    let minLength = Infinity;
 
-    for (let end = 0; end < S.length; end++){
-        count [s[end]]++;
-        
-        while (count['0'] > 0 && count['1'] > 0 && count['2'] > 0) {
-            minLength = Math.min(minLength, end - start + 1);
-            count[S[start]]--;
-            start++
-        }
-    }
-    return minLength === Infinity ? - 1 : minLength;
-}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let d = 2;
+let rotateArr = arr.slice(d)
+
+console.log(rotateArr);
