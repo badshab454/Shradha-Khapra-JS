290aa534325015836c40cd0e85f469c34f94c8e0
Badsha Hossain <badshab454@gmail.com>
2024-01-23 12:55:38 +0600

    join method

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var name1 = ["Sanjay", "Aman", "Rehman"];
 var name2 = ["Karan", "Bishnu"];
-var name3 = ["Shahrukh", "Khan"]
+var name3 = ["Shahrukh", "Khan"];
 
 var name4 = name1.concat(name2, name3);
 
-var name11 = name1.join("-");
+var name11 = name1.join('');
 
 console.log(name11);
