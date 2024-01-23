d24ec6988999163a6afa70400bc1b5ef8804a18b
Badsha Hossain <badshab454@gmail.com>
2024-01-23 11:44:03 +0600

    replace method

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let arr = [1, 2, 3, 4, 5, 6, 7];
-let d = 2;
-let rotateArr = arr.slice(d)
+var name = "Bodsha";
 
-console.log(rotateArr);
+var fixName = name.replace(/o/g, 'a')
+
+console.log(fixName);
