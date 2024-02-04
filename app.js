2dd64178a9811e990443f544f32e1824351fec9e
Badsha Hossain <badshab454@gmail.com>
2024-02-04 11:10:51 +0600

    hash plus

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-return [Math.min(...arr), Math.max(...arr)]
+function hashPlusCount(str) {
+   let arr = str.split("");
+   let hash = 0;
+   let plus = 0;
+   for (let i = 0; i < arr.length; i++){
+	if(arr[i] === "#"){
+		hash++;
+	} else {
+		plus++
+	}
+   }
+   return [hash, plus];
+}
+
+console.log(hashPlusCount("###+"));
